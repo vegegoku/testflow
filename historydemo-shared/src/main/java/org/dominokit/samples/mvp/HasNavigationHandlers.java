81d4e9c69e26fe30d28db08bdc9e70ea06c57216
Ahmad bawaneh <akabme@gmail.com>
2021-02-25 00:35:06 +0200

    test

--- historydemo-shared/src/main/java/org/dominokit/samples/mvp/HasNavigationHandlers.java
+++
@@ -1,8 +0,0 @@
-package org.dominokit.samples.mvp;
-
-public interface HasNavigationHandlers {
-    void onGoHome();
-    void onPageSelected(int pageIndex);
-
-    void onSearch(String name, String status);
-}
